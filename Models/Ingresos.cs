6a407c406d71982d4f0d06e94b687fe1905e8cc3
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-02 10:38:20 -0400

    Agregando el modelo

---
+++ Models/Ingresos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba_Yokaira.Models
+{
+    public class Ingresos
+    {
+        public int IngresoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public int Fecha { get; set; }
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public string Concepto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        [Range(1, 5000)]
+        public int Monto { get; set; }
+    }
+}
